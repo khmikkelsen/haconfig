835f1d81173bad1154f6e75ac4979a9f2d4304f9
khm <kh.mikkelsen@hotmail.com>
2021-09-24 12:15:10 +0200

    To avoid turning off the lights if 'on' state is held for longer than wait time, sensor 'off' will trigger the delay.

--- appdaemon/apps/motion_sensor.py
+++ appdaemon/apps/motion_sensor.py
@@ -1,5 +1,7 @@
+import datetime
 import warnings
 import appdaemon.plugins.hass.hassapi as hass
+from typing import List
 
 
 # noinspection PyAttributeOutsideInit
@@ -17,6 +19,8 @@ class Motion(hass.Hass):
             self.light_group = args['light_group']
             self.mode_input = args['mode_input']
             # optional args
+            if self.args.get('logger'):
+                self.logger = self.get_user_log(self.args['logger'])
             self._scene_morning = args.get('scene_morning')
             self._scene_day = args.get('scene_day')
             self._scene_evening = args.get('scene_evening')
@@ -29,11 +33,17 @@ class Motion(hass.Hass):
             self.wait_night = args.get('wait_night', 60)
             self.off_transition = args.get('off_transition', 15.0)
             self.on_transition = args.get('on_transition', 0.7)
-            # handler from last trigger
-            self._handle = None
-            self._last_trigger_time = None
-            self._mode_changed_handler = self.listen_state(self.mode_changed_cb, self.mode_input)
-            self._motion_handler = self.listen_state(self.motion, self.motion_sensor, new='on')
+            self._offset_evening = args.get('offset_evening', '15:00')
+            self._offset_morning = args.get('offset_morning', '0')
+            # handlers
+            self._motion_run_handle = None
+            self._motion_off_run_handle = None
+            self._mode_changed_handle = None
+            self._motion_off_listener = None
+            self._motion_listener = self.motion_listener
+            self._mode_changed_listener = self.mode_changed_listener
+            # vars
+            self._last_trigger_ts = None
 
         except (TypeError, ValueError) as e:
             self.log('Incomplete configuration', level="ERROR")
@@ -41,26 +51,32 @@ class Motion(hass.Hass):
 
     async def motion(self, entity, attribute, old, new, kwargs):
         lx = await self.get_luminosity()
+        self._last_trigger_ts = await self.get_now_ts()
         if lx <= await self.min_lx():
-            self.reset_handle()
-            self._handle = await self.create_task(self.motion_run())
+            self.reset_motion_handle()
+            self._motion_run_handle = await self.create_task(self.motion_run())
 
     async def motion_run(self, **kwargs):
         if await self.is_local_control():
             supported_colors = await self.get_state(self.light_group, attribute='supported_color_modes')
             mode = await self.get_mode()
-            wait = kwargs.get('wait', getattr(self, f'wait_{mode}'))
             if supported_colors == ['onoff']:
                 await self.turn_on(self.light_group)
             else:
                 await self.turn_on(getattr(self, f'scene_{mode}'), transition=self.on_transition)
-            self._last_trigger_time = await self.get_now_ts()
-            await self.sleep(wait)
-            if await self.is_local_control():
-                await self.turn_off(self.light_group, transition=self.off_transition)
+            self._motion_off_listener = self.motion_listener_off
+            
+    async def motion_off(self, entity, attribute, old, new, kwargs):
+        self._motion_off_run_handle = await self.create_task(self.motion_off_run())
 
-    async def get_mode(self):
-        return await self.get_state(self.mode_input)
+    async def motion_off_run(self, **kwargs):
+        mode = await self.get_mode()
+        wait = kwargs.get('wait', getattr(self, f'wait_{mode}'))
+        await self.sleep(wait)
+        self.log(f'finished waiting {wait} in {mode}')
+        if await self.is_local_control():
+            await self.turn_off(self.light_group, transition=self.off_transition)
+        await self.cancel_listen_state(self._motion_off_listener)
 
     async def get_luminosity(self):
         return float(await self.get_state(self.lx_sensor))
@@ -73,22 +89,43 @@ class Motion(hass.Hass):
         else:
             return self._min_lx
 
+    async def mode_changed_cb(self, entity, attribute, old, new, kwargs):
+        self._mode_changed_handle = await self.create_task(self.mode_changed_task())
+
+    async def mode_changed_task(self, **kwargs):
+        mode = await self.get_mode()
+        scene = getattr(self, f'scene_{mode}')
+        lights_state = await self.get_state(self.light_group)
+        if lights_state == 'on':
+            try:
+                self.log(f'mode_changed to scene: {scene}')
+                await self.turn_on(self.blocker)
+                limit = getattr(self, f'wait_{mode}')
+                await self.turn_on(scene, transition=1200 if 1200 < limit else limit)
+                # test env
+                # await self.turn_on(scene, transition=15)
+                await self.sleep(1201)
+                await self.turn_off(self.blocker)
+                self._motion_run_handle = await self.create_task(self.motion_run(wait=await self.remainder()))
+            except (Exception, BaseException) as e:
+                raise e
+
+    async def remainder(self):
+        try:
+            wait = getattr(self, f"wait_{await self.get_state(self.mode_input)}")
+            diff = await self.get_now_ts() - self._last_trigger_ts
+            remainder = wait - diff if wait - diff < wait else 0
+            self.log(f'wait:{wait} - diff:{diff} = {remainder}')
+        except (Exception, BaseException) as e:
+            remainder = 0
+            self.log(e)
+        return remainder
+
     async def is_local_control(self):
         return (self.blocker and await self.get_state(self.blocker) == 'off') or self.blocker is None
 
-    async def mode_changed_cb(self, entity, attribute, old, new, kwargs):
-        scene = getattr(self, f'scene_{await self.get_state(self.mode_input)}')
-        self.log(f'mode_changed to scene: {scene}')
-        lights_state = await self.get_state(self.light_group)
-        if lights_state == 'on':
-            await self.turn_on(self.blocker)
-            await self.turn_on(scene, transition=120)
-            await self.sleep(121)
-            await self.turn_off(self.blocker)
-            await self.sleep(1)
-            diff = getattr(self, f"wait_{await self.get_state(self.mode_input)}") - await self.get_now_ts() - self._last_trigger_time
-            self.log(f'diff: {diff}')
-            await self.motion_run(wait=diff)
+    async def get_mode(self):
+        return await self.get_state(self.mode_input)
 
     @property
     def scene_morning(self):
@@ -118,10 +155,71 @@ class Motion(hass.Hass):
         else:
             return self.light_group
 
-    def reset_handle(self):
-        if self._handle:
-            if not self._handle.done():
-                self._handle.cancel()
+    @property
+    def motion_listener(self):
+        return self.listen_state(self.motion, self.motion_sensor, new='on', old='off')
+
+    @property
+    def motion_listener_off(self):
+        return self.listen_state(self.motion_off, self.motion_sensor, new='off', old='on', oneshot=True)
+
+    @property
+    def mode_changed_listener(self):
+        return self.listen_state(self.mode_changed_cb, self.mode_input)
+
+    @property
+    def external_control_on_listener(self):
+        return self.listen_event(
+            self.external_control_on_event, 
+            'call_service', 
+            domain='light', 
+            service='turn_on')
+
+    @property
+    def external_control_off_listener(self):
+        return self.listen_event(
+            self.external_control_off_event,
+            'call_service',
+            domain='light',
+            service='turn_off')
+        
+    def event_contains_lights(self, data):
+        try:
+            entity_id = data['service_data']['entity_id'] 
+            return entity_id in self.get_lights or entity_id == self.get_lights
+        except Exception:
+            return False
+
+    async def external_control_off_event(self, event_name, data, kwargs):
+        if self.event_contains_lights(data):
+            await self.cancel_listen_state(self._motion_listener)
+            self._motion_listener = self.motion_listener
+            self.log(f'External control turned off')
+            await self.turn_off(self.blocker)
+
+    async def external_control_on_event(self, event_name, data, kwargs):
+        if self.event_contains_lights(data):
+            await self.cancel_listen_state(self._motion_listener)
+            self._motion_listener = self.motion_listener
+            self.log(f'External control turned on')
+            await self.turn_on(self.blocker)
+
+    @property
+    def get_lights(self) -> List[str]:
+        return self.get_state(self.light_group, attribute='entity_id')
+
+    def reset_motion_handle(self):
+        if self._motion_run_handle:
+            if not self._motion_run_handle.done():
+                self._motion_run_handle.cancel()
+        if self._motion_off_run_handle:
+            if not self._motion_off_run_handle.done():
+                self._motion_off_run_handle.cancel()
+
+    def reset_mode_changed_handle(self):
+        if self._mode_changed_handle:
+            if not self._mode_changed_handle.done():
+                self._mode_changed_handle.cancel()
 
     def ad_api_all_apps(self):
         objs = self.get_ad_api().AD.app_management.objects
